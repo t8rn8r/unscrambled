e52ef75fc372c4406640feccd36e300c253adafe
t8rn8r <wintermydog@gmail.com>
2022-03-05 20:29:00 -0600

    i don't know why i even make commits at this point...'

--- dictionary_trie.py
+++ dictionary_trie.py
@@ -1,9 +1,12 @@
 import json
 import pickle
+from nltk.corpus import words
 
 # could use this instead: https://tutorialedge.net/compsci/data-structures/getting-started-with-tries-in-python/
 
 # I actually used this: https://albertauyeung.github.io/2020/06/15/python-trie.html/
+
+
 class TrieNode:
     def __init__(self, char):
         self.char = char
@@ -11,25 +14,26 @@ class TrieNode:
         self.counter = 0
         self.children = {}
 
-class Trie(object): 
-    
+
+class Trie(object):
+
     def __init__(self):
         self.root = TrieNode('')
         self.codex = {}
-    
-    def insert(self, words):
-        
-        for word in words:
 
-            node = self.root
+    def insert(self, word):
 
+        node = self.root
+
+        if type(word) == type('this is a hack!'):
+            # sort the word's letters in alphabetical order to reduce mess
             sorted_word = "".join(sorted(word))
-
+            # add the unsorted word to the codex under its sorted entry
             if sorted_word in self.codex:
                 self.codex[sorted_word].append(word)
             else:
                 self.codex[sorted_word] = [word]
-
+            # actually insert the word, character by character
             for char in sorted_word:
                 if char in node.children:
                     node = node.children[char]
@@ -37,16 +41,27 @@ class Trie(object):
                     new_node = TrieNode(char)
                     node.children[char] = new_node
                     node = new_node
-
             # this marks where we've run out of characters in a word
             node.is_end = True
-
-            # TODO put an actual score in here using word frequency
+            # TODO put an actual score in here using word frequency or something
+            node.counter += 1
+        
+        if type(word) == type(['this','is','also','a','hack']):
+            sorted_phrase = sorted(word)
+            # no need for a codex here!
+            for word in sorted_phrase:
+                if word in node.children:
+                    node = node.children[word]
+                else:
+                    new_node = TrieNode(word)
+                    node.children[word] = new_node
+                    node = new_node
+            node.is_end = True
             node.counter += 1
 
 
     def dfs(self, node, prefix):
-        
+
         if node.is_end:
             self.output.append((prefix + node.char, node.counter))
 
@@ -54,9 +69,9 @@ class Trie(object):
             self.dfs(child, prefix + node.char)
 
     def query(self, prefix):
-        
+
         self.output = []
-        node = self.root 
+        node = self.root
 
         for char in prefix:
             if char in node.children:
@@ -68,11 +83,25 @@ class Trie(object):
 
         return sorted(self.output, key=lambda prefix: prefix[1], reverse=True)
 
+    def dfs_phrase(self, node, prefix):
+        if len(node.children) == 0:
+            self.output.append(prefix + [node.char])
+
+        for child in node.children.values():
+            self.dfs_phrase(child, prefix + [node.char])
     
-    def contains(self, string):
-        
+    def query_phrases(self):
         self.output = []
         node = self.root 
+
+        for word in node.children:
+            self.dfs_phrase(node.children[word], [])
+
+        return self.output
+
+    def contains(self, string):
+
+        node = self.root
 
         for char in string:
             if char in node.children:
@@ -83,34 +112,20 @@ class Trie(object):
         return node.is_end
 
 
-def main():
-    trie = Trie()
-    trie.insert(["a", "ab", "abc", "ac", "abcd", "acd", "abd", "aabd"])
-
-    print(trie.contains("ab"))
-
-
 def make_dict():
-    
-    words = Trie()
-    word_codex = {}
 
-    with open("words_dictionary_sorted.json") as infile:
-        
-        input_dict = json.load(infile)
+    words_trie = Trie()
+    word_list = words.words()
+    word_list = [x.lower() for x in word_list]
+    word_list = set(word_list)
 
-        words.insert(input_dict)
-        word_codex = words.getCodex()
+    for word in word_list:
+        words_trie.insert(word)
 
-    # https://docs.python.org/3/library/pickle.html 
+    # https://docs.python.org/3/library/pickle.html
     with open("words_trie.data", "wb") as outfile:
         pickle.dump(words, outfile, pickle.HIGHEST_PROTOCOL)
 
-    with open("words_codex.data", "wb") as outfile:
-        pickle.dump(word_codex, outfile, pickle.HIGHEST_PROTOCOL)
-
-
-
 
 if __name__ == '__main__':
-    main()
+    make_dict()
--- unscrambler.py
+++ unscrambler.py
@@ -2,22 +2,99 @@ import pickle
 import collections
 from more_itertools import powerset
 from dictionary_trie import TrieNode, Trie
+from wordfreq import word_frequency
 
 
-text = "".join(sorted("tiwashencneton"))
+# global_text = "".join(sorted("tiwashencneton"))
+global_text = "".join(sorted("tiwashencneton"))
+
+phrases = Trie()
+
+
+def get_words(text, allwords):
+    words = Trie()
+
+    # Figure out all the possible words made from every combination of text
+    combos = set(reversed(list(powerset(text))))
+    n_combos = len(combos)
+    count = 0
+    # iterate through all combos from longest to shortest
+    # print()
+    for combo in combos:
+        combo = "".join(combo)
+        # count += 1
+        # percent = count*100/n_combos
+        # print("Checking all possible combinations...\t%02.1f%% done! (%i out of %i)" % (
+        #     percent, count, n_combos), end='\r')
+
+        if allwords.contains(combo):
+            words.insert(combo)
+
+    return words
+
+
+def get_phrases(text, words_dict, current_phrase=[]):
+
+    word_list = sorted(list(words_dict), key=len, reverse=True)
+
+    if len(text) == 0:
+        phrases.insert(current_phrase)
+        print("%i phrases found!" % len(phrases.query_phrases()), end='\r')
+
+    for word in word_list:
+        words_dict.pop(word)
+
+        new_text = text
+        for c in word:
+            new_text = new_text.replace(c, '', 1)
+
+        new_words_trie = Trie()
+        for w in words_dict:
+            new_words_trie.insert(w)
+        new_words = dict(get_words(new_text, new_words_trie).query(''))
+
+        current_phrase.append(word)
+
+        get_phrases(new_text, new_words, current_phrase)
+
+        current_phrase.pop()
 
 
 def main():
 
     allwords = Trie()
-    words = Trie()
-
     with open("words_trie.data", "rb") as infile:
         allwords = pickle.load(infile)
 
-    combos = reversed(list(powerset(text)))
-    for combo in combos:
-        print(combo)
+    words = get_words(global_text, allwords)
+
+    # Get the frequency of each of the words
+    alphabetical_word_list = words.query('')
+    word_freq = {}
+    count = 0
+    print()
+    for alphabetical_word in alphabetical_word_list:
+        # isolate the word from the "score"
+        alphabetical_word = alphabetical_word[0]
+        count += 1
+        percent = count*100/len(alphabetical_word_list)
+        print("Checking word frequencies...\t\t%02.1f%% done! (%i out of %i)" %
+              (percent, count, len(alphabetical_word_list)), end='\r')
+        # First, you have to "decode" the words from the codex
+        codex_words = allwords.codex[alphabetical_word]
+        for word in codex_words:
+            # This gives each word a "frequency" proportional to its frequency and its length
+            # word_freq[w] = word_frequency(w, 'en', wordlist='small', minimum=0) * pow(1000, len(w))
+            word_freq[word] = word_frequency(
+                word, 'en', wordlist='small', minimum=0) * pow(10, len(word))
+    # sort by my custom scoring for each word
+    word_freq = dict(
+        sorted(word_freq.items(), key=lambda item: item[1], reverse=True))
+    print()
+    print("%i potential words found!" % len(word_freq))
+    # clear phrases (just in case!)
+    phrases = []
+    get_phrases(global_text, dict(words.query('')))
 
     print()
 
